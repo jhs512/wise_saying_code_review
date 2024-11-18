823c802093021af6f84339cea089a019777ad5da
류대현 <eogus1027@naver.com>
2024-11-18 17:29:17 +0900

    종료 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        String command = scanner.nextLine();
+
+        if (command.equals("종료")) {
+            return;
+        }
     }
 }
