0fcfa9092eb904723623ebc5326504fd3df15be4
Hoon <xianse10@gmail.net>
2024-11-18 16:51:01 +0900

    3단계 4단계 완료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
+        int uniqueid = 0;
+
         while(true){
             System.out.print("명령 )");
             String order = scanner.nextLine();
@@ -20,12 +22,12 @@ public class Main {
                 System.out.println("작가 : ");
                 String writer = scanner.nextLine();
 
-                System.out.println("명언 : " + wiseSaying);
-                System.out.println("작가 : " + writer);
+                uniqueid++;
+                System.out.println(uniqueid + " 번 명언이 등록 되었습니다.");
                 System.out.println();
             } else if (order.equals("종료")) {
                 System.out.println("프로그램을 종료합니다.");
-                break; // 반복문 종료
+                break;
             } else {
                 System.out.println("알 수 없는 명령입니다.");
                 System.out.println();
