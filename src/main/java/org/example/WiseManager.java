f5be6b0ad8b4a2889b108421651ce93d2aae1f7d
류대현 <eogus1027@naver.com>
2024-11-18 22:06:31 +0900

    명언 및 마지막 명언 번호 저장 기능 구현

--- src/main/java/org/example/WiseManager.java
+++ src/main/java/org/example/WiseManager.java
@@ -1,14 +1,21 @@
 package org.example;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Optional;
 
 public class WiseManager {
     private ArrayList<Wise> wises = new ArrayList<>();
     private int index = 1;
+    private final String BASE_PATH = "db/wiseSaying/";
 
     public void applyWise(String wise, String author) {
-        wises.add(new Wise(index, author, wise));
+        Wise w = new Wise(index, author, wise);
+        wises.add(w);
+        saveWise(w);
+        saveLastId();
+
         System.out.println(index++ + "번 명언이 등록되었습니다.");
     }
 
@@ -31,5 +38,35 @@ public class WiseManager {
         } else {
             return null;
         }
+    }
+
+    private void saveWise(Wise wise) {
+        String path = BASE_PATH + index + ".json";
+        try (FileOutputStream output = new FileOutputStream(path)) {
+            output.write(getJsonString(wise).getBytes());
+        } catch (IOException e) {
+            System.out.println("파일 출력 에러");
+        }
+    }
+
+    public void saveLastId() {
+        String path = BASE_PATH + "lastId.txt";
+        try (FileOutputStream output = new FileOutputStream(path)) {
+            output.write(String.valueOf(index).getBytes());
+        } catch (IOException e) {
+            System.out.println("파일 출력 에러");
+        }
+    }
+
+    private String getJsonString(Wise wise) {
+        String json = """
+                {
+                  "id": %d,
+                  "content": "%s",
+                  "author": "%s"
+                }
+                """;
+
+        return String.format(json, wise.index, wise.wise, wise.author);
     }
 }
