de9c8dd77d8671ce30fa5cdf5167f8c0e8816d75
류대현 <eogus1027@naver.com>
2024-11-18 17:31:32 +0900

    명언 등록 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String command = scanner.nextLine();
 
-        if (command.equals("종료")) {
-            return;
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals("종료")) {
+                break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String wise = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+            }
         }
     }
 }
