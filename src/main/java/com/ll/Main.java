696efa6b79651161aa3dc029642e8358cc684769
Hoon <xianse10@gmail.net>
2024-11-18 16:41:56 +0900

    2단계 완성

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -9,8 +9,30 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
-        System.out.println("명령 )");
-        String input = scanner.next();
+        while(true){
+            System.out.print("명령 )");
+            String order = scanner.nextLine();
+
+            if(order.equals("등록")){
+                System.out.println("명언 : ");
+                String wiseSaying = scanner.nextLine();
+
+                System.out.println("작가 : ");
+                String writer = scanner.nextLine();
+
+                System.out.println("명언 : " + wiseSaying);
+                System.out.println("작가 : " + writer);
+                System.out.println();
+            } else if (order.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break; // 반복문 종료
+            } else {
+                System.out.println("알 수 없는 명령입니다.");
+                System.out.println();
+            }
+        }
+
+        scanner.close();
 
     }
 }
