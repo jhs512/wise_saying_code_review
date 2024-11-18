68f19e7d16d66d8da7ddc264af9e74c74e3ddf9b
vdvhk12 <vdvhk12@gmail.com>
2024-11-18 17:38:14 +0900

    Step5 - 목록

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -35,17 +35,23 @@ public class Main {
                 break;
             else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String autor = br.readLine();
+                String content = br.readLine();
 
                 System.out.print("작가 : ");
-                String content = br.readLine();
-                al.add(new WiseSaying(Id, autor, content));
+                String author = br.readLine();
+                al.add(new WiseSaying(Id, author, content));
 
                 // 등록시 생성된 명언번호 출력
                 System.out.println(Id + "번 명언이 등록되었습니다.");
 
                 // 등록할 때 마다 생성되는 명언번호가 증가
                 Id++;
+            } else if (cmd.equals("목록")) {      // 목록
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("--------------------");
+                for(WiseSaying ws : al) {
+                    System.out.println(ws.id + " / " + ws.author + " / " + ws.content);
+                }
             } else {
                 System.out.println("올바르지 않은 명령어 입니다.");
             }
