74f72b1a1db2133b6032b28765633fa885ab996f
kdo_1999/mac <kdo_1999@naver.com>
2024-11-20 16:00:50 +0900

    refactor: WiseSayingController run() 추가 - 기존에 WiseSayingApplication에 있었던 run() 로직 분리

--- src/main/java/WiseSayingApplication.java
+++ src/main/java/WiseSayingApplication.java
@@ -13,55 +13,19 @@ public class WiseSayingApplication {
 		= (WiseSayingController)container.get(WiseSayingController.class.getSimpleName());
 
 	public static void main(String[] args) {
-		new WiseSayingApplication().run();
+		init();
+		wiseSayingController.run();
 	}
 
 	public static ConcurrentHashMap<String, Object> init() {
 		ConcurrentHashMap<String, Object> container = new ConcurrentHashMap<>();
 		WiseSayingRepository wiseSayingRepository = new WiseSayingRepositoryImpl();
 		WiseSayingService wiseSayingService = new WiseSayingServiceImpl(wiseSayingRepository);
-		WiseSayingController wiseSayingController = new WiseSayingController(wiseSayingService);
+		WiseSayingController wiseSayingController = new WiseSayingController(wiseSayingService, new Scanner(System.in));
 
 		container.put(WiseSayingRepositoryImpl.class.getSimpleName(), wiseSayingRepository);
 		container.put(WiseSayingServiceImpl.class.getSimpleName(), wiseSayingService);
 		container.put(WiseSayingController.class.getSimpleName(), wiseSayingController);
 		return container;
-	}
-
-	public void run() {
-		while(true) {
-			System.out.println("== 명언 앱 ==");
-			System.out.print("명령) ");
-
-			Scanner sc = new Scanner(System.in);
-
-			String select = sc.nextLine();
-
-			if (select.equals("종료")) {
-				sc.close();
-				wiseSayingController.build();
-				break;
-			}
-
-			switch (select) {
-				case "등록":
-					wiseSayingController.add(sc);
-					break;
-				case "목록":
-					wiseSayingController.findAll();
-					break;
-				case "삭제":
-					wiseSayingController.delete(sc);
-					break;
-				case "수정":
-					wiseSayingController.update(sc);
-					break;
-				case "빌드":
-					wiseSayingController.build();
-					break;
-				default:
-					System.out.println("잘못된 명령입니다.");
-			}
-		}
 	}
 }
--- src/main/java/wisesaying/controller/WiseSayingController.java
+++ src/main/java/wisesaying/controller/WiseSayingController.java
@@ -6,12 +6,14 @@ import wisesaying.service.WiseSayingService;
 
 public class WiseSayingController {
 	private final WiseSayingService wiseSayingService;
+	private final Scanner sc;
 
-	public WiseSayingController(WiseSayingService wiseSayingService) {
+	public WiseSayingController(WiseSayingService wiseSayingService, Scanner sc) {
 		this.wiseSayingService = wiseSayingService;
+		this.sc = sc;
 	}
 
-	public void add(Scanner sc) {
+	public void add() {
 		System.out.print("명언 : ");
 		String wiseSaying = sc.nextLine();
 
@@ -29,11 +31,11 @@ public class WiseSayingController {
 		wiseSayingService.findAll();
 	}
 
-	public void update(Scanner sc) {
+	public void update() {
 		wiseSayingService.update(sc);
 	}
 
-	public void delete(Scanner sc) {
+	public void delete() {
 		System.out.print("삭제?id = ");
 
 		Long targetId = sc.nextLong();
@@ -44,5 +46,42 @@ public class WiseSayingController {
 
 	public void build() {
 		wiseSayingService.build();
+	}
+
+	public void run() {
+		while (true) {
+			System.out.println("== 명언 앱 ==");
+			System.out.print("명령) ");
+
+			Scanner sc = new Scanner(System.in);
+
+			String select = sc.nextLine();
+
+			if (select.equals("종료")) {
+				sc.close();
+				build();
+				break;
+			}
+
+			switch (select) {
+				case "등록":
+					add();
+					break;
+				case "목록":
+					findAll();
+					break;
+				case "삭제":
+					delete();
+					break;
+				case "수정":
+					update();
+					break;
+				case "빌드":
+					build();
+					break;
+				default:
+					System.out.println("잘못된 명령입니다.");
+			}
+		}
 	}
 }
