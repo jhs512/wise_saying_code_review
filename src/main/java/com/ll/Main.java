1931204c23ab35e31eab5717a1fe006f05ddccd1
oungsi2000 <oungsi1000@gmail.com>
2024-11-20 10:07:40 +0900

    feat:Controller 클래스 완성
    게시판의 삭제, 생성, 수정, 목록 출력, 빌드
    의 기능을 가진 클래스 생성을 완료하였습니다
    
    추가된 클래스 - Controller - 비즈니스 로직
    - Console : 사용자 UI 및 입출력을 담당합니다
    Controller 에서 호출합니다

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,343 @@
 package com.ll;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+enum Command {
+    CREATE,
+    LIST_ALL,
+    DELETE,
+    UPDATE,
+    BUILD,
+    EXIT
+}
+
+class Console {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static Command getCommand() {
+        while (true) {
+            System.out.print("명령) ");
+            String command = getInput();
+
+            if (Objects.equals(command, "등록")) return Command.CREATE;
+            if (Objects.equals(command, "삭제")) return Command.DELETE;
+            if (Objects.equals(command, "목록")) return Command.LIST_ALL;
+            if (Objects.equals(command, "수정")) return Command.UPDATE;
+            if (Objects.equals(command, "빌드")) return Command.BUILD;
+            if (Objects.equals(command, "종료")) return Command.EXIT;
+        }
+    }
+
+    public static String getInput() {
+        return scanner.nextLine().trim();
+    }
+
+    public static void printWelcome() {
+        System.out.println("== 명언 앱 ==");
+    }
+
+    public static void print(String args) {
+        System.out.print(args);
+    }
+}
+
+class Controller {
+    static String FILE_DIR = "src/main/resources/db/wiseSaying";
+    public List<Map<String, String>> data;
+    public Boolean isTestMode;
+
+    Controller(Boolean isTestMode) {
+        data = load();
+        this.isTestMode = isTestMode;
+    }
+
+    Controller() {
+        this(false);
+    }
+
+    private File[] getFiles() {
+        File directory = new File(FILE_DIR);
+
+        if (directory.exists() && directory.isDirectory()) {
+            return directory.listFiles();
+        } else {
+            return new File[0];
+        }
+    }
+
+    private StringBuilder loadStringFile() throws IOException{
+        StringBuilder data = new StringBuilder();
+
+        data.append("[");
+        for (File file : getFiles()) {
+            if (file.getName().equals("data.json")) continue; //data.json 패스
+            if (file.getName().equals("lastId.txt")) continue; //lastId 패스
+
+            try (Scanner scanner = new Scanner(new File(STR."\{FILE_DIR}/\{file.getName()}"))){
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    data.append(line);
+                    data.append(",");
+                }
+
+            } catch (IOException e) {
+                throw new IOException("파일 로딩에 오류가 발생했습니다", e);
+            }
+        }
+        data.append("]");
+        return data;
+    }
+
+    private List<Map<String, String>> strToMap(String strData) throws IOException {
+        try {
+            //정규 표현식으로 {} 안에 있는 내용을 리스트로 반환
+            String regex = "\\{(.|\\n)*?\\}";
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(strData);
+            List<Map<String, String>> elements = new ArrayList<>();
+
+            //각 {} 요소 파싱
+            while (matcher.find()) {
+                Map<String, String> item = new HashMap<>();
+                String strItem = matcher.group();
+                String[] fields = strItem.split(",");
+
+                // 각 {} 요소의 키-값 파싱
+                for (String field : fields) {
+                    try {
+                        String key = field.split(":")[0].
+                            replace("\"", "").
+                            replace("{", "").
+                            replace("}", "").
+                            trim();
+                        String value = field.split(":")[1].
+                            replace("\"", "").
+                            replace("{", "").
+                            replace("}", "").
+                            trim();
+                        item.put(key,value);
+                    } catch (IndexOutOfBoundsException _) {
+                    }
+                }
+                elements.add(item);
+            }
+
+            return elements;
+        } catch (Exception e) {
+            throw new IOException("올바르지 않은 파일 형식", e);
+        }
+
+    }
+
+    private List<Map<String, String>> load() {
+        try {
+            String strData = loadStringFile().toString();
+            return strToMap(strData);
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private String mapToStr(List<Map<String, String>> maps) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[\n");
+        for (Map<String, String> map : maps) {
+            sb.append("\t{\n");
+            sb.append("\t\t\"id\" : \"");
+            sb.append(map.get("id"));
+            sb.append("\"");
+            sb.append(",\n");
+
+            sb.append("\t\t\"content\" : \"");
+            sb.append(map.get("content"));
+            sb.append("\"");
+            sb.append(",\n");
+
+            sb.append("\t\t\"author\" : \"");
+            sb.append(map.get("author"));
+            sb.append("\"\n");
+
+            sb.append("\t},\n");
+        }
+        sb.deleteCharAt(sb.length() - 2);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public void build() {
+        String strData = mapToStr(data);
+
+        try (FileWriter writer = new FileWriter(STR."\{FILE_DIR}/data.json");) {
+            writer.write(strData);
+        } catch (IOException e) {
+            throw new RuntimeException("빌드에 실패했습니다", e);
+        }
+    }
+
+    public void save(Map<String, String> data) {
+        List<Map<String, String>> mapData = new ArrayList<>();
+        mapData.add(data);
+
+        //id.json 형식으로 저장
+        String strData = mapToStr(mapData).replace("[", "").replace("]", "");
+        try (FileWriter writer = new FileWriter(STR."\{FILE_DIR}/\{data.get("id")}.json");) {
+            writer.write(strData);
+        } catch (IOException e) {
+            throw new RuntimeException("저장에 실패했습니다", e);
+        }
+        //lastId.txt 저장
+        try (FileWriter writer = new FileWriter(STR."\{FILE_DIR}/lastId.txt");) {
+            writer.write(getLastID());
+        } catch (IOException e) {
+            throw new RuntimeException("저장에 실패했습니다", e);
+        }
+
+    }
+    private String getLastID() {
+        try {
+            int max = 0;
+            for (Map<String, String> d : data) {
+                if (Integer.parseInt(d.get("id")) > max) {
+                    max = Integer.parseInt(d.get("id"));
+                }
+            }
+            return Integer.toString(max);
+        } catch (NoSuchElementException e) {
+            return "0";
+        }
+    }
+
+    public void create() {
+        Console.print("명언 : ");
+        String wise = Console.getInput();
+        Console.print("작가 : ");
+        String author = Console.getInput();
+
+        int intItemID = Integer.parseInt(getLastID()) + 1;
+        String itemID = Integer.toString(intItemID);
+
+        Map<String, String> item = new HashMap<>();
+        item.put("id", itemID);
+        item.put("content", wise);
+        item.put("author", author);
+        data.add(item);
+        save(item);
+        Console.print(itemID + "번 명언이 등록되었습니다.\n");
+
+    }
+
+    public void listUp() {
+        Console.print("번호 / 작가 / 명언\n" +
+            "----------------------\n");
+        StringBuilder sb = new StringBuilder();
+        for (Map<String, String> d : data) {
+            sb.append(d.get("id"));
+            sb.append(" / ");
+            sb.append(d.get("content"));
+            sb.append(" / ");
+            sb.append(d.get("author"));
+            sb.append("\n");
+        }
+        Console.print(sb.toString());
+    }
+
+    private void modifyLastId(String id) {
+        try (FileWriter writer = new FileWriter(STR."\{FILE_DIR}/lastId.txt");) {
+            writer.write(id);
+        } catch (IOException e) {
+            throw new RuntimeException("수정에 실패했습니다", e);
+        }
+    }
+    public void delete(String id) {
+        Map<String, String> target = new HashMap<>();
+        for (Map<String, String> d : data) {
+            if (Objects.equals(d.get("id"), id)) {
+                target = d;
+                break;
+            }
+        }
+        data.remove(target);
+
+        File file = new File(STR."\{FILE_DIR}/\{id}.json");
+        if (file.exists()) { // 파일이 존재하는 경우
+            if (file.delete()) {
+                Console.print(STR."\{id}번 명언이 삭제되었습니다.\n");
+            } else {
+                Console.print("파일 삭제에 실패했습니다\n");
+            }
+        } else {
+            Console.print(STR."\{id}번 명언은 존재하지 않습니다.\n");
+        }
+
+        //최댓값이 달라졌을 때 수정
+        if (Integer.parseInt(getLastID()) < Integer.parseInt(id)) {
+            modifyLastId(id);
+        }
+    }
+
+    public void update(String id) {
+
+        //탐색
+        Map<String, String> target = new HashMap<>();
+        int index = 0;
+        for (Map<String, String> d : data) {
+            if (Objects.equals(d.get("id"), id)) {
+                target = d;
+                break;
+            }
+            index += 1;
+        }
+
+        if (target.isEmpty()) {
+            Console.print(STR."\{id}번 명언은 존재하지 않습니다.\n");
+            return;
+        }
+
+        Console.print(STR."명언(기존) : \{target.get("content")}\n");
+        Console.print("명언 : ");
+        String newContent = Console.getInput();
+
+        Console.print(STR."\n작가(기존) : \{target.get("author")}\n");
+        Console.print("작가 : ");
+        String newAuthor = Console.getInput();
+
+        Map<String, String> newData = new HashMap<>();
+        newData.put("id", id);
+        newData.put("content", newContent);
+        newData.put("author", newAuthor);
+
+        data.set(index, newData);
+
+        //파일 덮어씌우기
+        save(newData);
+
+    }
+}
+
+class App {
+    Controller controller = new Controller();
+
+    public void execute() {
+        Console.printWelcome();
+        while (true) {
+            Command command = Console.getCommand();
+
+            if (command == Command.EXIT) break;
+            if (command == Command.BUILD) controller.build();
+
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+    }
+}
+
+
+
+
---
+++ src/test/java/com/ll/ApplicationTest.java
@@ -0,0 +1,68 @@
+package com.ll;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class ApplicationTest {
+
+  @Test
+  public void fileLoadTest () {
+    Controller controller = new Controller();
+    assertThat(controller.data).hasSize(2);
+    assertThat(controller.data.getFirst().get("id")).isEqualTo("1");
+    assertThat(controller.data.getFirst().get("content")).isEqualTo("명언 1");
+  }
+
+  @Test
+  public void fileBuildTest () {
+    Controller controller = new Controller();
+    controller.build();
+  }
+
+  @Test
+  public void createTest() {
+    Controller controller = new Controller();
+    ByteArrayInputStream in1 = new ByteArrayInputStream("엄\n준".getBytes());
+    System.setIn(in1);
+    controller.create();
+    assertThat(controller.data.getLast().get("content")).isEqualTo("엄");
+  }
+
+  @Test
+  public void listUpTest() {
+    // ByteArrayOutputStream을 이용하여 출력을 가로채기
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputStream));
+
+    Controller controller = new Controller();
+    controller.listUp();
+
+    String actualOutput = outputStream.toString();
+
+    assertThat(actualOutput).isEqualTo("번호 / 작가 / 명언\n" +
+        "----------------------");
+  }
+
+  @Test
+  public void deleteTest() {
+    Controller controller = new Controller();
+    controller.delete("5");
+    assertThat(controller.data.toString()).isEqualTo("[]");
+  }
+
+  @Test
+  public void updateTest() {
+
+    ByteArrayInputStream in1 = new ByteArrayInputStream("수정\n완\n".getBytes());
+    System.setIn(in1);
+
+    Controller controller = new Controller();
+    controller.update("5");
+
+    assertThat(controller.data.getFirst().get("content")).isEqualTo("수정");
+  }
+}
