838adf3d27ee603291ca66fd3b5527d5e54b87e3
nodonghui <163218508+nodonghui@users.noreply.github.com>
2024-11-22 16:30:49 +0900

    add wiseSaying

---
+++ src/main/java/com/llwiseSaying/Application.java
@@ -0,0 +1,14 @@
+package com.llwiseSaying;
+
+import com.llwiseSaying.Controller.WiseSayingController;
+
+import java.io.IOException;
+
+public class Application {
+
+    static App app=new App();
+    public static void main(String[] args) throws IOException {
+
+        app.run();
+    }
+}
