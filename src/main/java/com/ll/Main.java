3cf89d274d3ed88a61d0614eb9f618108213453f
Gyeonghun Min <riverpower6@naver.com>
2024-12-31 14:31:12 +0900

    up to 1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,17 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        switch (command) {
+            case "종료":
+                return;
+        }
     }
 }
