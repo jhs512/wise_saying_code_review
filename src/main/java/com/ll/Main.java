405ad66d3c3b41df25d5c8ec65d94f11898facc8
Gyeonghun Min <riverpower6@naver.com>
2024-12-31 14:38:27 +0900

    up to 3

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
         String command;
+        int num = 1;
 
         while (true) {
             System.out.print("명령) ");
@@ -17,6 +18,8 @@ public class Main {
                     String content = scanner.nextLine();
                     System.out.print("작가 : ");
                     String author = scanner.nextLine();
+                    WiseSaying wiseSaying = new WiseSaying(num, author, content);
+                    System.out.println(num + "번 명언이 등록되었습니다.");
                     break;
                 case "종료":
                     return;
---
+++ src/main/java/com/ll/WiseSaying.java
@@ -0,0 +1,37 @@
+package com.ll;
+
+public class WiseSaying {
+    private long id;
+    private String author;
+    private String content;
+
+    public WiseSaying(long id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
